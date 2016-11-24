b43aeab52d7f67577ec9ca876ba817b734114b44
Sujit Singh <sujit.extentia@gmail.com>
2016-11-24 14:36:02 +0530

    changed server port config to dynamic

--- app.js
+++ app.js
@@ -202,5 +202,6 @@ app.use(function(err, req, res, next) {
  
 module.exports = app;
 
-app.listen(3001);
-console.log("Server listening on port 3000.");
+app.listen(process.env.PORT || 3000, function(){
+  console.log('listening on' + process.env.PORT);
+});
