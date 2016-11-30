91a7b23125b2db47be671ecce57b951083a67b10
Sujit Singh <sujit.extentia@gmail.com>
2016-11-30 16:23:56 +0530

    added logging to file

--- app.js
+++ app.js
@@ -182,7 +182,17 @@ var sendMail = function(params){
 		}
 	}
 
+	var fs = require('fs');
+var util = require('util');
+var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
+var log_stdout = process.stdout;
 
+console.log = function(d) { //
+  log_file.write(util.format(d) + '\n');
+  log_stdout.write(util.format(d) + '\n');
+};
+	
+	
 // error handlers
 
 // development error handler
